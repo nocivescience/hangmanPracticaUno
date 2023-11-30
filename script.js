fba4b546879998976181d3543bcd831556b004c0
nocive.science <nocive.science@gmail.com>
2023-11-30 03:26:45 -0300

    agregado el final-message

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const wrongLettersEl = document.getElementById('wrong-letters');
 const playAgainBtn = document.getElementById('play-button');
 const popup = document.getElementById('popup-container');
 const notification = document.getElementById('notification');
+const finalMessage = document.getElementById('final-message');
 const words=['application','programming','interface','wizard'];
 let selectedWord=words[Math.floor(Math.random()*words.length)];
 const correctLetters=[];

e8872fddfca10ff80dd7eb3454d04fd2f7c28cbd
nocive.science <nocive.science@gmail.com>
2023-11-30 03:19:55 -0300

    terminadas las constantes del script

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const wordEl = document.getElementById('word');
 const wrongLettersEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-button');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification');
+const words=['application','programming','interface','wizard'];
+let selectedWord=words[Math.floor(Math.random()*words.length)];
+const correctLetters=[];
+const wrongLetters=[];
+// show hidden word
